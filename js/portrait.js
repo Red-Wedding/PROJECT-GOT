07ba00dd17361fcc71be44d1bf27293265a504b7
Pál Gábor <gaborpal1992@gmail.com>
2018-11-30 20:13:10 +0100

    müködj légyszives

---
+++ js/portrait.js
@@ -0,0 +1,14 @@
+function namesandportraits() {
+  var tomb = '';
+  for ( var i = 0; i < gameOfThronesCharacters.length; i += 1) {
+    tomb += `<div class='card'> <img src=/${gameOfThronesCharacters[i].portrait}>
+  <p>${gameOfThronesCharacters[i].name}</p>
+ </div> `;
+  }
+  document.querySelector('.container').innerHTML = tomb;
+}
+
+
+namesandportraits();
+
+
