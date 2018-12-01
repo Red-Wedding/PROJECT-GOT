2813a831c3bc770a17919c4f35dff6cd81f3e85b
Szabo Ruben <s.r.tailor777@gmail.com>
2018-12-01 17:26:49 +0100

    final touch01

--- js/charactersRuben.js
+++ js/charactersRuben.js
@@ -73,15 +73,14 @@ function footer() {
   var footerElement = document.getElementById('footer');
   var row =
     `
-  <p><b>OFFICIAL GAME OF THRONES WEBSITE - team RED WEDDING</b></p>
-  <p>Follow us</p>
-  <a href="http://facebook.com"><img src="/img/facebook.png" alt=""/></a>
-  <a href="http://instagram.com"
-    ><img src="/img/instagram.png" alt=""
-  /></a>
-  <a href="http://youtube.com"><img src="/img/youtube.png" alt=""/></a>
-  <a href="http://twitter.com"><img src="/img/twitter.svg" alt=""/></a>
+    <p><b>OFFICIAL GAME OF THRONES WEBSITE - team RED WEDDING</b></p>
+    <p>Follow us</p>
+    <a href="http://facebook.com"><img src="/img/facebook.png" alt="facebook" /></a>
+    <a href="http://instagram.com"><img src="/img/Instagram.png" alt="Instagram" /></a>
+    <a href="http://youtube.com"><img src="/img/youtube.png" alt="youtube" /></a>
+    <a href="http://twitter.com"><img src="/img/twitter.svg" alt="twitter" /></a>
   `;
+  footerElement.innerHTML = row;
 }
 
 function deleteRow() {
