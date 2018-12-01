9c5e4dd0e906647bc83ab006ef33e1580d911c2b
Fanni Tormasi <tormasifanni@gmail.com>
2018-12-01 09:36:00 +0100

    adding files to feature-Fanni

---
+++ js/alive_dead.js
@@ -0,0 +1,16 @@
+function deadLiving() {
+  var tableRow = '';
+  for (var i = 0; i < gameOfThronesCharacters.length; i++) {
+    if (gameOfThronesCharacters[i].dead === true) {
+      tableRow +=
+                `
+                <tr>
+                    <td> ${gameOfThronesCharacters[i].name} </td>
+                    </td>    
+                </tr>        
+            `;
+    }
+    document.querySelector('#dead-alive').innerHTML = tableRow;
+  }
+}
+deadLiving();
