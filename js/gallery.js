f32942e782a2d7254be4b7a2b1596f80db11ba81
Pál Gábor <gaborpal1992@gmail.com>
2018-12-01 18:35:19 +0100

    elkezdtem összerakni a dolgokat és megy a hotfix ezerrel

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,7 @@
 function pictures() {
   var tomb = '';
   for ( var i = 0; i < gameOfThronesCharacters.length; i += 1) {
-    tomb += `<div class='picture'> <img src=/${gameOfThronesCharacters[i].picture} alt='Jon Snow havatlapátol a Burger King előtt.'>
+    tomb += `<div class='picture'> <img src=/${gameOfThronesCharacters[i].picture} alt='Jon Snow havatlapátol a Burger King-nél.'>
 
    </div> `;
   }
