c0054dffb190c3561f5ea5820a339176749287f9
Pál Gábor <gaborpal1992@gmail.com>
2018-12-02 00:18:05 +0100

    'nagyjábol kész minden a filjaimra figyeljetek és külön megvan akkor a menü css

--- js/portrait.js
+++ js/portrait.js
@@ -2,8 +2,8 @@ function namesandportraits() {
   var tomb = '';
   for ( var i = 0; i < gameOfThronesCharacters.length; i += 1) {
     tomb += `<div class='card'> <img src=/${gameOfThronesCharacters[i].portrait}>
-  <p>${gameOfThronesCharacters[i].name}</p>
- </div> `;
+  <div><p>${gameOfThronesCharacters[i].name}</p>
+ </div></div> `;
   }
   document.querySelector('.portraitcontainer').innerHTML = tomb;
 }
